ee0d790b9d244ad4be64a064c7d56518c703b679
Yago Peixinho <peixinhoyago@gmail.com>
2022-11-11 17:33:22 -0300

    :construction: wip: add new progress into login page auth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import { api } from "../../api/axios";
+
 export default function Login() {
-  return <h1 className="teste">Página de Login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleRegister(value) {
+    const item = {
+      email: email,
+      password: password,
+    };
+
+    await api.post("/users/auth", item);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(ev) => setEmail(ev.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(ev) => {
+            setPassword(ev.target.value);
+          }}
+        />
+
+        {JSON.stringify(email)}
+        {JSON.stringify(password)}
+        <button
+          onClick={() => {
+            handleRegister();
+          }}
+        >
+          Enviar
+        </button>
+      </div>
+    </div>
+  );
 }
