387c7771ba8c5bf699e1e27c7de804b716adcda4
Yago Peixinho <peixinhoyago@gmail.com>
2022-11-21 13:48:33 -0300

    :sparkles: feat: the application now have a global header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import { useContext } from "react";
-import { useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logoSmall from "../assets/images/logos/logo-small.svg";
 import { AppContext } from "../contexts/store";
+import homeIconDark from "../assets/images/icons/home-icon-dark.svg";
 
 export default function Header() {
   const { store } = useContext(AppContext);
@@ -17,15 +18,25 @@ export default function Header() {
       <div className="nav-header">
         <img src={logoSmall} alt="Logo do APP" />
         <ul>
-          <li>Home</li>
+          <NavLink to="home" className="nav-link">
+            <li>
+              <span>
+                <img
+                  src={homeIconDark}
+                  alt="Uma casa simbolizando a página Home"
+                />
+              </span>
+              Home
+            </li>
+          </NavLink>
         </ul>
       </div>
 
       <div className="user-info">
-        <div className="box-user-image"></div>
         <div>
           <h3>{store.user.user.name}</h3>
         </div>
+        <div className="box-user-image"></div>
       </div>
     </div>
   );
